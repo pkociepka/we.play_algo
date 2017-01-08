7fb0dea8adfa650e7726fe367a0437800059faa6
Piotr Ociepka <pkociepka@users.noreply.github.com>
2017-01-08 20:28:29 +0100

    Fix for case-sensitive SQL servers.

--- db/db_hook.py
+++ db/db_hook.py
@@ -40,17 +40,17 @@ class DB_Connector:
                   for x in self._execute(track_sql)}
         # [print(t) for t in tracks.values()]
 
-        artist_sql = """SELECT artist.id, artist.name, artist_by_track.Track_id FROM Artist
-                                    JOIN artist_by_track ON artist.id = artist_by_track.Artist_id
-                                    WHERE artist_by_track.Track_id IN ('%s')""" % "','".join(track_ids)
+        artist_sql = """SELECT Artist.id, artist.name, artist_by_track.track_id FROM Artist
+                                    JOIN artist_by_track ON artist.id = artist_by_track.artist_id
+                                    WHERE artist_by_track.track_id IN ('%s')""" % "','".join(track_ids)
         track_artists = self._execute(artist_sql)
         artists = {x[0]: Artist(x[0], x[1]) for x in track_artists}
         for t_a in track_artists:
             tracks[t_a[2]].add_author(artists[t_a[0]])
 
-        genre_sql = """SELECT genre.id, genre_by_track.Track_id FROM Genre
-                                            JOIN genre_by_track ON genre.id = genre_by_track.Genre_id
-                                            WHERE genre_by_track.Track_id IN ('%s')""" % "','".join(track_ids)
+        genre_sql = """SELECT Genre.id, genre_by_track.track_id FROM Genre
+                                            JOIN genre_by_track ON genre.id = genre_by_track.genre_id
+                                            WHERE genre_by_track.track_id IN ('%s')""" % "','".join(track_ids)
         track_genres = self._execute(genre_sql)
         genres = {x[0]: Genre(x[0]) for x in track_genres}
         for t_g in track_genres:
@@ -62,7 +62,7 @@ class DB_Connector:
     ###################
 
     def find_tracks_by_user(self, user):
-        sql = "SELECT `Track_id` FROM `track_by_user` WHERE `User_id` LIKE %s" % user.id
+        sql = "SELECT `track_id` FROM `track_by_user` WHERE `user_id` LIKE %s" % user.id
         track_ids = [track_id[0] for track_id in self._execute(sql)]
         return self._build_tracks(track_ids)
 
@@ -83,12 +83,12 @@ class DB_Connector:
     ######################
 
     def find_tracks_by_artist(self, artist_id):
-        sql = "SELECT `Track_id` FROM `artist_by_track` WHERE `Artist_id` LIKE '%s'" % artist_id
+        sql = "SELECT `track_id` FROM `artist_by_track` WHERE `artist_id` LIKE '%s'" % artist_id
         track_ids = [x[0] for x in self._execute(sql)]
         return self._build_tracks(track_ids)
 
     def find_tracks_by_genre(self, genre_id):
-        sql = "SELECT `Track_id` FROM `genre_by_track` WHERE `Genre_id` LIKE %s" % genre_id
+        sql = "SELECT `track_id` FROM `genre_by_track` WHERE `genre_id` LIKE %s" % genre_id
         track_ids = [x[0] for x in self._execute(sql)]
         return self._build_tracks(track_ids)
 
@@ -102,14 +102,14 @@ class DB_Connector:
         for genre in user.genres:
             self.put_genre(genre)
             sql.append(
-                "INSERT INTO `genre_by_user` (User_id, Genre_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE user_id=user_id" % (
+                "INSERT INTO `genre_by_user` (user_id, genre_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE user_id=user_id" % (
                 user.id, genre.name))
         for artist in user.authors:
             self.put_artist(artist)
-            sql.append("INSERT INTO `artist_by_user` (User_id, Artist_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE user_id=user_id" % (user.id, artist.id))
+            sql.append("INSERT INTO `artist_by_user` (user_id, artist_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE user_id=user_id" % (user.id, artist.id))
         for track in user.tracks:
             self.put_track(track)
-            sql.append("INSERT INTO `track_by_user` (User_id, Track_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE user_id=user_id" % (user.id, track.id))
+            sql.append("INSERT INTO `track_by_user` (user_id, track_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE user_id=user_id" % (user.id, track.id))
         self._commit(sql)
 
     def put_genre(self, genre: Genre):
@@ -124,7 +124,7 @@ class DB_Connector:
         sql = ["INSERT INTO `Track` (id, `name`, danceability, energy, instrumentalness, valence) VALUES ('%s', '%s', %s, %s, %s, %s) ON DUPLICATE KEY UPDATE id=id" %
                     (track.id, track.name, track.params.danceability, track.params.energy, track.params.instrumentalness, track.params.valence)]
         for artist in track.authors:
-            sql.append("INSERT INTO `artist_by_track` (Artist_id, Track_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE track_id=track_id" % (artist.id, track.id))
+            sql.append("INSERT INTO `artist_by_track` (artist_id, track_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE track_id=track_id" % (artist.id, track.id))
         for genre in track.genres:
-            sql.append("INSERT INTO `genre_by_track` (Track_id, Genre_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE track_id=track_id" % (track.id, genre.name))
+            sql.append("INSERT INTO `genre_by_track` (track_id, genre_id) VALUES ('%s', '%s') ON DUPLICATE KEY UPDATE track_id=track_id" % (track.id, genre.name))
         self._commit(sql)
